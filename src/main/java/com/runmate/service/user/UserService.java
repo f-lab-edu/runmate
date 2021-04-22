952c34fc45fc4846dcba42a7ed2944a89ec43ac8
bugdebugging <annyousung@gmail.com>
2021-04-22 17:43:22 +0900

    FEAT:UserService(modify,delete)구현

--- src/main/java/com/runmate/service/user/UserService.java
+++ src/main/java/com/runmate/service/user/UserService.java
@@ -2,9 +2,40 @@ package com.runmate.service.user;
 
 import com.runmate.domain.dto.AuthRequest;
 import com.runmate.domain.user.User;
+import com.runmate.repository.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
-public interface UserService {
-    boolean join(User user);
-    boolean login(AuthRequest request);
-    User getUser(String email);
+import javax.transaction.Transactional;
+
+@Service
+@Transactional
+public class UserService{
+    @Autowired
+    private UserRepository userRepository;
+
+    public boolean join(User user) {
+        if(user!=null && userRepository.findByEmail(user.getEmail())==null){
+            userRepository.save(user);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean login(AuthRequest request) {
+        User user=userRepository.findByEmail(request.getEmail());
+        if(user==null || !user.getPassword().equals(request.getPassword()))
+            return false;
+        return true;
+    }
+
+    public User getUser(String email) {
+        return userRepository.findByEmail(email);
+    }
+    public void modify(User user){
+        userRepository.save(user);
+    }
+    public boolean delete(String email){
+        return userRepository.deleteByEmail(email)==1;
+    }
 }
