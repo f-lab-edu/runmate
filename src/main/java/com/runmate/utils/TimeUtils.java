a203668d92a6494082c90c912915696532a95797
pkalsh <pkalsh345@gmail.com>
2021-04-27 21:58:45 +0900

    FEAT: long type의 초 단위와 LocalTime type을 서로 변환하는 Util 클래스 개발

---
+++ src/main/java/com/runmate/utils/TimeUtils.java
@@ -0,0 +1,20 @@
+package com.runmate.utils;
+
+import java.time.LocalTime;
+
+public class TimeUtils {
+    private static final int HOUR_SECOND_UNIT = 3600;
+    private static final int MINUTE_SECOND_UNIT = 60;
+
+    public static long timeToSeconds(LocalTime time) {
+        return time.getHour() * HOUR_SECOND_UNIT + time.getMinute() * MINUTE_SECOND_UNIT + time.getSecond();
+    }
+
+    public static LocalTime secondsToTime(long seconds) {
+        int hour = (int) (seconds / HOUR_SECOND_UNIT);
+        int minute = (int) ((seconds % HOUR_SECOND_UNIT) / MINUTE_SECOND_UNIT);
+        int second = (int) ((seconds % HOUR_SECOND_UNIT) % MINUTE_SECOND_UNIT);
+
+        return LocalTime.of(hour, minute, second);
+    }
+}
