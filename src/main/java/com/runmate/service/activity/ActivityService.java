6705541f06439ef37ed0181e0c4073ad5f855d12
pkalsh <pkalsh345@gmail.com>
2021-04-28 07:36:53 +0900

    FEAT: 연도별 통계 기록 조회 기능 구현

--- src/main/java/com/runmate/service/activity/ActivityService.java
+++ src/main/java/com/runmate/service/activity/ActivityService.java
@@ -2,13 +2,20 @@ package com.runmate.service.activity;
 
 import com.runmate.domain.activity.Activities;
 import com.runmate.domain.activity.Activity;
+import com.runmate.domain.dto.ActivityDto;
+import com.runmate.domain.dto.ActivityStatisticsDto;
 import com.runmate.domain.user.User;
 import com.runmate.repository.activity.ActivityRepository;
 import com.runmate.repository.user.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import javax.transaction.Transactional;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -25,9 +32,24 @@ public class ActivityService {
 
         Activities activities = new Activities(user.getActivities());
 
-        if (user.canUpgrade(activities.calTotalDistance())) {
+        if (user.canUpgrade(activities.calculateTotalDistance())) {
             user.upgrade();
             userRepository.save(user);
         }
+    }
+
+    @Transactional(readOnly = true)
+    public ActivityStatisticsDto findYearlyStatistics(String email, int year) {
+        User user = userRepository.findByEmail(email);
+
+        LocalDate fromDate = LocalDate.of(year, 1, 1);
+        LocalDateTime from = LocalDateTime.of(fromDate, LocalTime.MIN);
+
+        LocalDate toDate = LocalDate.of(year, 12, 31);
+        LocalDateTime to = LocalDateTime.of(toDate, LocalTime.MAX);
+
+        Activities activities = new Activities(activityRepository.findAllByUserAndBetweenDates(user.getId(), from, to));
+
+        return activities.toStatistics();
     }
 }
