64c22645b70b6f9ed9ac6792158e883a3ba68f30
pkalsh <pkalsh345@gmail.com>
2021-04-28 08:11:00 +0900

    TEST: 이번달 동안의 활동 기록 검색 테스트 추가 작성

--- src/test/java/com/runmate/repository/ActivityRepositoryTest.java
+++ src/test/java/com/runmate/repository/ActivityRepositoryTest.java
@@ -11,9 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.transaction.Transactional;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
+import java.time.*;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -22,6 +20,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 @Transactional
 public class ActivityRepositoryTest {
+    public static final int CURRENT_YEAR = LocalDate.now().getYear();
+    public static final Month CURRENT_MONTH = LocalDate.now().getMonth();
     @Autowired
     ActivityRepository activityRepository;
     @Autowired
@@ -50,13 +50,35 @@ public class ActivityRepositoryTest {
 
     @DisplayName("올해 동안의 활동 기록 검색")
     @Test
-    void findAllByUserAndBetweenDates() {
+    void findActivitiesDuringCurrentYear() {
         //given
-        int year = LocalDate.now().getYear();
-        LocalDate fromDate = LocalDate.of(year, 1, 1);
+        LocalDate fromDate = LocalDate.of(CURRENT_YEAR, 1, 1);
         LocalDateTime from = LocalDateTime.of(fromDate, LocalTime.MIN);
 
-        LocalDate toDate = LocalDate.of(year, 12, 31);
+        LocalDate toDate = LocalDate.of(CURRENT_YEAR, 12, 31);
+        LocalDateTime to = LocalDateTime.of(toDate, LocalTime.MAX);
+
+        //when
+        List<Activity> activities = activityRepository.findAllByUserAndBetweenDates(user.getId(), from, to);
+
+        //then
+        assertThat(activities.get(0).getDistance()).isEqualTo(10.5f);
+        assertThat(activities.get(0).getRunningTime()).isEqualTo(LocalTime.of(0, 50, 45));
+        assertThat(activities.get(0).getCalories()).isEqualTo(500);
+
+        assertThat(activities.get(1).getDistance()).isEqualTo(8.2f);
+        assertThat(activities.get(1).getRunningTime()).isEqualTo(LocalTime.of(0, 38, 52));
+        assertThat(activities.get(1).getCalories()).isEqualTo(418);
+    }
+
+    @DisplayName("이번달 동안의 활동 기록 검색")
+    @Test
+    void findActivitiesDuringCurrentMonth() {
+        //given
+        LocalDate fromDate = LocalDate.of(CURRENT_YEAR, CURRENT_MONTH, 1);
+        LocalDateTime from = LocalDateTime.of(fromDate, LocalTime.MIN);
+
+        LocalDate toDate = LocalDate.of(CURRENT_YEAR, CURRENT_MONTH, CURRENT_MONTH.length(Year.isLeap(CURRENT_YEAR)));
         LocalDateTime to = LocalDateTime.of(toDate, LocalTime.MAX);
 
         //when
