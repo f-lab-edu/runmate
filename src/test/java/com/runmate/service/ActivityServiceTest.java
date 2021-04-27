2d94d5b25af15ec8847aa194976005335c9918c4
pkalsh <pkalsh345@gmail.com>
2021-04-28 07:56:34 +0900

    TEST: Repository의 행위 Mocking 추

--- src/test/java/com/runmate/service/ActivityServiceTest.java
+++ src/test/java/com/runmate/service/ActivityServiceTest.java
@@ -17,11 +17,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.LocalTime;
-import java.time.Month;
+import java.time.*;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -43,6 +42,7 @@ public class ActivityServiceTest {
 
     User user;
     Activity completedActivity;
+    List<Activity> activities;
 
     @BeforeEach
     public void setUp() {
@@ -66,6 +66,20 @@ public class ActivityServiceTest {
         user.setGrade(Grade.UNRANKED);
         user.setEmail("ppp@ppp.com");
         user.setActivities(activities);
+
+        Activity activity3 = Activity.builder()
+                .distance(10.5f)
+                .runningTime(LocalTime.of(0, 50, 45))
+                .calories(500)
+                .build();
+
+        Activity activity4 = Activity.builder()
+                .distance(8.2f)
+                .runningTime(LocalTime.of(0, 38, 52))
+                .calories(418)
+                .build();
+
+        this.activities = Arrays.asList(activity3, activity4);
     }
 
     @Test
@@ -101,6 +115,19 @@ public class ActivityServiceTest {
     @DisplayName("연도별 활동 통계 조회")
     @Test
     public void findYearlyStatistics() {
+        //given
+        Mockito.when(userRepository.findByEmail(user.getEmail()))
+                .thenReturn(user);
+
+        LocalDate fromDate = LocalDate.of(CURRENT_YEAR, 1, 1);
+        LocalDateTime from = LocalDateTime.of(fromDate, LocalTime.MIN);
+
+        LocalDate toDate = LocalDate.of(CURRENT_YEAR, 12, 31);
+        LocalDateTime to = LocalDateTime.of(toDate, LocalTime.MAX);
+
+        Mockito.when(activityRepository.findAllByUserAndBetweenDates(user.getId(), from, to))
+                .thenReturn(activities);
+
         //when
         ActivityStatisticsDto yearlyStatistics = service.findYearlyStatistics(user.getEmail(), CURRENT_YEAR);
 
@@ -108,27 +135,53 @@ public class ActivityServiceTest {
         assertThat(yearlyStatistics.getCount()).isEqualTo(2);
         assertThat(yearlyStatistics.getDistance()).isEqualTo(18.7f);
         assertThat(yearlyStatistics.getRunningTime()).isEqualTo(RunningTime.of(1, 29, 37));
-        assertThat(yearlyStatistics.getPace()).isEqualTo(LocalTime.of(0, 4, 48));
+        assertThat(yearlyStatistics.getAveragePace()).isEqualTo(LocalTime.of(0, 4, 47));
         assertThat(yearlyStatistics.getCalories()).isEqualTo(918);
     }
 
     @DisplayName("어떤 활동도 없는 연도의 연도별 통계 조회")
     @Test
     public void findYearlyStatisticsWhenHasNotAnyActivities() {
+        //given
+        int year = 1960;
+        Mockito.when(userRepository.findByEmail(user.getEmail()))
+                .thenReturn(user);
+
+        LocalDate fromDate = LocalDate.of(year, 1, 1);
+        LocalDateTime from = LocalDateTime.of(fromDate, LocalTime.MIN);
+
+        LocalDate toDate = LocalDate.of(year, 12, 31);
+        LocalDateTime to = LocalDateTime.of(toDate, LocalTime.MAX);
+        Mockito.when(activityRepository.findAllByUserAndBetweenDates(user.getId(), from, to))
+                .thenReturn(Collections.emptyList());
+
         //when
-        ActivityStatisticsDto yearlyStatistics = service.findYearlyStatistics(user.getEmail(), 1960);
+        ActivityStatisticsDto yearlyStatistics = service.findYearlyStatistics(user.getEmail(), year);
 
         //then
         assertThat(yearlyStatistics.getCount()).isEqualTo(0);
         assertThat(yearlyStatistics.getDistance()).isEqualTo(0f);
-        assertThat(yearlyStatistics.getRunningTime()).isEqualTo(LocalTime.of(0, 0, 0));
-        assertThat(yearlyStatistics.getPace()).isEqualTo(LocalTime.of(0, 0, 0));
+        assertThat(yearlyStatistics.getRunningTime()).isEqualTo(RunningTime.from(LocalTime.of(0, 0, 0)));
+        assertThat(yearlyStatistics.getAveragePace()).isEqualTo(LocalTime.of(0, 0, 0));
         assertThat(yearlyStatistics.getCalories()).isEqualTo(0);
     }
 
     @DisplayName("월별 활동 통계 조회")
     @Test
     public void findMonthlyStatistics() {
+        //given
+        Mockito.when(userRepository.findByEmail(user.getEmail()))
+                .thenReturn(user);
+
+        LocalDate fromDate = LocalDate.of(CURRENT_YEAR, CURRENT_MONTH, 1);
+        LocalDateTime from = LocalDateTime.of(fromDate, LocalTime.MIN);
+
+        LocalDate toDate = LocalDate.of(CURRENT_YEAR, CURRENT_MONTH, CURRENT_MONTH.length(Year.isLeap(CURRENT_YEAR)));
+        LocalDateTime to = LocalDateTime.of(toDate, LocalTime.MAX);
+
+        Mockito.when(activityRepository.findAllByUserAndBetweenDates(user.getId(), from, to))
+                .thenReturn(activities);
+
         //when
         ActivityStatisticsDto monthlyStatistics =
                 service.findMonthlyStatistics(user.getEmail(), CURRENT_YEAR, CURRENT_MONTH);
@@ -137,7 +190,7 @@ public class ActivityServiceTest {
         assertThat(monthlyStatistics.getCount()).isEqualTo(2);
         assertThat(monthlyStatistics.getDistance()).isEqualTo(18.7f);
         assertThat(monthlyStatistics.getRunningTime()).isEqualTo(RunningTime.of(1, 29, 37));
-        assertThat(monthlyStatistics.getPace()).isEqualTo(LocalTime.of(0, 4, 48));
+        assertThat(monthlyStatistics.getAveragePace()).isEqualTo(LocalTime.of(0, 4, 47));
         assertThat(monthlyStatistics.getCalories()).isEqualTo(918);
     }
 
@@ -145,20 +198,48 @@ public class ActivityServiceTest {
     @Test
     public void findWeeklyStatistics() {
         //given
-        User newUser = new User();
+        LocalDate fromDate = LocalDate.of(2021, 4, 25);
+        LocalDate toDate = LocalDate.of(2021, 5, 1);
 
-        LocalDate from = LocalDate.of(2021, 4, 25);
-        LocalDate to = LocalDate.of(2021, 5, 1);
+        Mockito.when(userRepository.findByEmail(user.getEmail()))
+                .thenReturn(user);
+
+        LocalDateTime from = LocalDateTime.of(fromDate, LocalTime.MIN);
+        LocalDateTime to = LocalDateTime.of(toDate, LocalTime.MAX);
+
+        Mockito.when(activityRepository.findAllByUserAndBetweenDates(user.getId(), from, to))
+                .thenReturn(activities);
 
         //when
-        ActivityStatisticsDto weeklyStatistics = service.findWeeklyStatistics(newUser.getEmail(), from, to);
+        ActivityStatisticsDto weeklyStatistics = service.findWeeklyStatistics(user.getEmail(), fromDate, toDate);
 
         //then
         assertThat(weeklyStatistics.getCount()).isEqualTo(2);
         assertThat(weeklyStatistics.getDistance()).isEqualTo(18.7f);
         assertThat(weeklyStatistics.getRunningTime()).isEqualTo(RunningTime.of(1, 29, 37));
-        assertThat(weeklyStatistics.getPace()).isEqualTo(LocalTime.of(0, 4, 48));
+        assertThat(weeklyStatistics.getAveragePace()).isEqualTo(LocalTime.of(0, 4, 47));
         assertThat(weeklyStatistics.getCalories()).isEqualTo(918);
     }
 
+    @DisplayName("최근 활동 조회")
+    @Test
+    public void findRecentActivities() {
+        //given
+        int offset = 1;
+        int length = 5;
+
+        //when
+        List<ActivityDto> activities = service.findRecentActivities(user.getEmail(), offset, length);
+
+        //then
+        assertThat(activities.size()).isEqualTo(2);
+
+        assertThat(activities.get(0).getDistance()).isEqualTo(8.2f);
+        assertThat(activities.get(0).getRunningTime()).isEqualTo(LocalTime.of(0, 38, 52));
+        assertThat(activities.get(0).getCalories()).isEqualTo(418);
+
+        assertThat(activities.get(1).getDistance()).isEqualTo(10.5f);
+        assertThat(activities.get(1).getRunningTime()).isEqualTo(LocalTime.of(0, 50, 45));
+        assertThat(activities.get(1).getCalories()).isEqualTo(500);
+    }
 }
