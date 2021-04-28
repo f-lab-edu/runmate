b89fbc08712c42f84ba965cd016a4854edac095b
pkalsh <pkalsh345@gmail.com>
2021-04-28 11:28:00 +0900

    TEST: 정렬 기준을 받아 페이징 조회하는 테스트 작성
    - 최근 활동 기록 조회: 최근순으로 페이지당 5개 기록, 3개 반환
    - 최근 활동 기록 조회: 결과가 없는 페이지 요청시 빈 리스트 반환

--- src/test/java/com/runmate/repository/ActivityRepositoryTest.java
+++ src/test/java/com/runmate/repository/ActivityRepositoryTest.java
@@ -9,6 +9,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 import javax.transaction.Transactional;
 import java.time.*;
@@ -16,6 +18,7 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 @Transactional
@@ -92,5 +95,55 @@ public class ActivityRepositoryTest {
         assertThat(activities.get(1).getDistance()).isEqualTo(8.2f);
         assertThat(activities.get(1).getRunningTime()).isEqualTo(LocalTime.of(0, 38, 52));
         assertThat(activities.get(1).getCalories()).isEqualTo(418);
+    }
+
+    @DisplayName("최근 활동 기록 조회: 최근순으로 페이지당 5개 기록, 3개 반환")
+    @Test
+    void findActivitiesWithPagination() {
+        //given
+        String email = "sung@sung.com";
+        User user = userRepository.findByEmail(email);
+
+        int offset = 0;
+        int limit = 5;
+        Sort sortByCreatedAt = Sort.by(Sort.Direction.DESC, "createdAt");
+
+        //when
+        List<Activity> activities = activityRepository.findAllByUserWithPagination(user.getId(), PageRequest.of(offset, limit, sortByCreatedAt));
+
+        //then
+        assertNotNull(activities);
+        assertThat(activities.size()).isEqualTo(3);
+
+        assertThat(activities.get(0).getDistance()).isEqualTo(128f);
+        assertThat(activities.get(0).getRunningTime()).isEqualTo(LocalTime.of(11, 47, 30));
+        assertThat(activities.get(0).getCalories()).isEqualTo(10555);
+
+        assertThat(activities.get(1).getDistance()).isEqualTo(120.195f);
+        assertThat(activities.get(1).getRunningTime()).isEqualTo(LocalTime.of(12, 15, 30));
+        assertThat(activities.get(1).getCalories()).isEqualTo(10526);
+
+        assertThat(activities.get(2).getDistance()).isEqualTo(80.195f);
+        assertThat(activities.get(2).getRunningTime()).isEqualTo(LocalTime.of(8, 15, 30));
+        assertThat(activities.get(2).getCalories()).isEqualTo(9024);
+    }
+
+    @DisplayName("최근 활동 기록 조회: 결과가 없는 페이지 요청시 빈 리스트 반환")
+    @Test
+    void givenNoResultForPage_WhenFindActivitiesWithPagination_ThenReturnEmptyList() {
+        //given
+        String email = "sung@sung.com";
+        User user = userRepository.findByEmail(email);
+
+        int offset = 3;
+        int limit = 5;
+        Sort sortByCreatedAt = Sort.by(Sort.Direction.DESC, "createdAt");
+
+        //when
+        List<Activity> activities = activityRepository.findAllByUserWithPagination(user.getId(), PageRequest.of(offset, limit, sortByCreatedAt));
+
+        //then
+        assertNotNull(activities);
+        assertThat(activities.size()).isEqualTo(0);
     }
 }
