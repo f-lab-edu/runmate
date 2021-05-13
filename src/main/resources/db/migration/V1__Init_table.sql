abca9416676769f50e203a1a3e05fbb54f76d9f0
pkalsh <pkalsh345@gmail.com>
2021-05-13 18:18:16 +0900

    CHORE: 초기화 데이터 추가

--- src/main/resources/db/migration/V1__Init_table.sql
+++ src/main/resources/db/migration/V1__Init_table.sql
@@ -2,6 +2,7 @@ drop table if exists user cascade;
 drop table if exists activity cascade;
 drop table if exists crew cascade;
 drop table if exists crew_user cascade;
+drop table if exists crew_join_request cascade;
 
 create table user
 (
@@ -52,6 +53,17 @@ create table crew_user
     foreign key (crew_id) references crew(id) on delete cascade
 ) default character set utf8;
 
+create table crew_join_request
+(
+    id          bigint primary key auto_increment,
+    user_id     bigint NOT NULL ,
+    crew_id     bigint NOT NULL ,
+    created_at  timestamp default CURRENT_TIMESTAMP,
+
+    foreign key (user_id) references user(id) on delete cascade ,
+    foreign key (crew_id) references crew(id) on delete cascade
+) default character set utf8;
+
 insert into user(email, introduction, name, password, si, gu, grade)
 values ('you@you.com', '메일 뛰자!', 'you', 1234, 'seoul', 'nowon', 'UNRANKED');
 
@@ -60,6 +72,12 @@ values ('ann@ann.com', '같이 뛰자!', 'ann', 1234, 'seoul', 'gangbuk', 'BRONZ
 
 insert into user(email, introduction, name, password, si, gu, grade)
 values ('sung@sung.com', '오래 뛰자!', 'sung', 1234, 'seoul', 'gangnam', 'SILVER');
+
+insert into user(email, introduction, name, password, si, gu, grade)
+values ('min@gmail.com', '친구 구해요!', 'kim', 1234, 'seoul', 'nowon', 'UNRANKED');
+
+insert into user(email, introduction, name, password, si, gu, grade)
+values ('jan@naver.com', '같이 뛰실분 쪽지 주세요', 'james', 1234, 'seoul', 'gangnam', 'BRONZE');
 
 insert into activity(user_id, distance, running_time, calories)
 values (1, 10.5, '00:50:45', 500);
@@ -78,6 +96,13 @@ values (3, 120.195, '12:15:30', 10526, addtime(now(), '10'));
 insert into activity(user_id, distance, running_time, calories, created_at)
 values (3, 128, '11:47:30', 10555, addtime(now(), '100'));
 
+insert into activity(user_id, distance, running_time, calories, created_at)
+values (5, 80.195, '08:15:30', 9024, now());
+insert into activity(user_id, distance, running_time, calories, created_at)
+values (5, 120.195, '12:15:30', 10526, addtime(now(), '10'));
+insert into activity(user_id, distance, running_time, calories, created_at)
+values (5, 128, '11:47:30', 10555, addtime(now(), '100'));
+
 insert into crew(description, si, gu, grade_limit, created_at)
 values ('첫 크루입니다.', 'seoul', 'gangbuk', 'BRONZE', now());
 
@@ -95,3 +120,11 @@ values (3, 2, 'ADMIN');
 
 insert into crew_user(user_id, crew_id, role)
 values (1, 3, 'ADMIN');
+
+insert into crew_join_request(user_id, crew_id)
+values (4, 3);
+
+insert into crew_join_request(user_id, crew_id)
+values (5, 1);
+insert into crew_join_request(user_id, crew_id)
+values (5, 2);
