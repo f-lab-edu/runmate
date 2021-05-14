8f34f79eef2374d6a0ec27b75f509dfc87161a94
bugdebugging <annyousung@gmail.com>
2021-05-14 21:57:33 +0900

    FEAT:지역조건에 따른 Crew검색 구현

--- src/main/java/com/runmate/repository/crew/CrewRepository.java
+++ src/main/java/com/runmate/repository/crew/CrewRepository.java
@@ -3,5 +3,6 @@ package com.runmate.repository.crew;
 import com.runmate.domain.crew.Crew;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface CrewRepository extends JpaRepository<Crew, Long> {
 }
