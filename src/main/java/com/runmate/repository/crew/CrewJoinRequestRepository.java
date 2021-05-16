dbd5fc28462079b18d139cacd7cda48c01b2c302
bugdebugging <annyousung@gmail.com>
2021-05-16 16:54:01 +0900

    FEAT:CrewJoinRequest Repository 구현

--- src/main/java/com/runmate/repository/crew/CrewJoinRequestRepository.java
+++ src/main/java/com/runmate/repository/crew/CrewJoinRequestRepository.java
@@ -1,7 +1,15 @@
 package com.runmate.repository.crew;
 
+import com.runmate.domain.crew.Crew;
 import com.runmate.domain.crew.CrewJoinRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-public interface CrewJoinRequestRepository extends JpaRepository<CrewJoinRequest,Long> {
+import java.util.List;
+
+public interface CrewJoinRequestRepository extends JpaRepository<CrewJoinRequest, Long> {
+    @Query("SELECT CJR FROM CrewJoinRequest CJR where CJR.crew =:crew")
+    List<CrewJoinRequest> findAllByCrewWithPageable(@Param("crew") Crew crew, Pageable request);
 }
