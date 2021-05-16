f5dd9178bbbe17da7e8c6699734e2b3ad47618bf
bugdebugging <annyousung@gmail.com>
2021-05-16 14:26:34 +0900

    FEAT,TEST:CrewJoinRequest 추가 기능 및 테스트

--- src/main/java/com/runmate/domain/crew/CrewUser.java
+++ src/main/java/com/runmate/domain/crew/CrewUser.java
@@ -1,13 +1,17 @@
 package com.runmate.domain.crew;
 
 import com.runmate.domain.user.User;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Getter
 @Entity
 @Table(name = "crew_user")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CrewUser {
 
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -26,4 +30,10 @@ public class CrewUser {
     @Column(name = "role")
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Builder
+    public CrewUser(User user,Crew crew){
+        this.user=user;
+        this.crew=crew;
+    }
 }
