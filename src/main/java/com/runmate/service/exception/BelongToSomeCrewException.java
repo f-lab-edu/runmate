21665afdd9dea109f0a07e443e1ca0fb5de2601c
bugdebugging <annyousung@gmail.com>
2021-05-17 21:51:47 +0900

    FEAT:BelongToSomeCrewException

---
+++ src/main/java/com/runmate/service/exception/BelongToSomeCrewException.java
@@ -0,0 +1,7 @@
+package com.runmate.service.exception;
+
+public class BelongToSomeCrewException extends RuntimeException{
+    public BelongToSomeCrewException(String message) {
+        super(message);
+    }
+}
