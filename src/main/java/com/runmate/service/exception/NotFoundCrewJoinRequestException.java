84d5d4bdbde6c5428fc0b34b31896181ddd2c2ae
bugdebugging <annyousung@gmail.com>
2021-05-17 21:56:56 +0900

    FEAT:NotFoundCrewJoinRequestException

---
+++ src/main/java/com/runmate/service/exception/NotFoundCrewJoinRequestException.java
@@ -0,0 +1,7 @@
+package com.runmate.service.exception;
+
+public class NotFoundCrewJoinRequestException extends RuntimeException{
+    public NotFoundCrewJoinRequestException(String message) {
+        super(message);
+    }
+}
