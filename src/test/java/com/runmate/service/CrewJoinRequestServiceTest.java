8f6092f9fbcb1e07ed23d0a278a676f2622bfab6
bugdebugging <annyousung@gmail.com>
2021-05-18 15:47:46 +0900

    REFACTOR:CrewJoinRequestServiceTest

--- src/test/java/com/runmate/service/CrewJoinRequestServiceTest.java
+++ src/test/java/com/runmate/service/CrewJoinRequestServiceTest.java
@@ -6,7 +6,9 @@ import com.runmate.domain.crew.CrewUser;
 import com.runmate.domain.user.Grade;
 import com.runmate.domain.user.User;
 import com.runmate.repository.crew.CrewJoinRequestRepository;
+import com.runmate.repository.crew.CrewRepository;
 import com.runmate.repository.crew.CrewUserRepository;
+import com.runmate.repository.user.UserRepository;
 import com.runmate.service.crew.CrewJoinRequestService;
 import com.runmate.service.exception.BelongToSomeCrewException;
 import com.runmate.service.exception.GradeLimitException;
@@ -32,35 +34,49 @@ public class CrewJoinRequestServiceTest {
     @MockBean
     CrewUserRepository crewUserRepository;
 
+    @MockBean
+    CrewRepository crewRepository;
+
+    @MockBean
+    UserRepository userRepository;
+
     @Autowired
     TextureFactory textureFactory;
 
     @Test
     public void When_SendRequest_UNRANKED_User_To_BRONZE_GradeLimit_Crew_Expect_GradeLimitException() {
-        //설정된 크루x, Request 보낸적도 없는 유저로 설정.
-        when(crewJoinRequestRepository.findCrewJoinRequestByCrewAndUser(any(Crew.class), any(User.class))).thenReturn(null);
-        when(crewUserRepository.findByCrewAndUser(any(Crew.class), any(User.class))).thenReturn(null);
-
+        final Long crewId = 1L;
+        final String email = "Lambda@Lambda.com";
         Crew crew = makeCrewWithGradeLimit(Grade.BRONZE);
-        User user = makeUserWithGrade(Grade.UNRANKED);
+        User user = makeUserWithGrade(email, Grade.UNRANKED);
+
+        crewJoinRequestRepositoryWillReturn(Optional.empty());
+        crewUserRepositoryWillReturn(Optional.empty());
+
+        crewRepositoryWillReturnCrew(crewId, crew);
+        userRepositoryWillReturnUser(email, user);
 
         //then
         assertThrows(GradeLimitException.class, () -> {
-            crewJoinRequestService.sendJoinRequest(crew, user);
+            crewJoinRequestService.sendJoinRequest(crewId, email);
         });
     }
 
     @Test
     public void When_SendRequest_DIA_User_To_BRONZE_GradeLimit_Crew_Expect_Call_RequestSave() {
-        //설정된 크루x, Request 보낸적도 없는 유저로 설정.
-        when(crewJoinRequestRepository.findCrewJoinRequestByCrewAndUser(any(Crew.class), any(User.class))).thenReturn(Optional.empty());
-        when(crewUserRepository.findByCrewAndUser(any(Crew.class), any(User.class))).thenReturn(Optional.empty());
-
+        final Long crewId = 1L;
+        final String email = "Lambda@Lambda.com";
         Crew crew = makeCrewWithGradeLimit(Grade.BRONZE);
-        User user = makeUserWithGrade(Grade.DIA);
+        User user = makeUserWithGrade(email, Grade.DIA);
+
+        crewJoinRequestRepositoryWillReturn(Optional.empty());
+        crewUserRepositoryWillReturn(Optional.empty());
+
+        crewRepositoryWillReturnCrew(crewId, crew);
+        userRepositoryWillReturnUser(email, user);
 
         //when
-        crewJoinRequestService.sendJoinRequest(crew, user);
+        crewJoinRequestService.sendJoinRequest(crewId, email);
 
         //then
         verify(crewJoinRequestRepository, atLeastOnce()).save(any(CrewJoinRequest.class));
@@ -68,47 +84,55 @@ public class CrewJoinRequestServiceTest {
 
     @Test
     public void When_SendRequest_Belonging_User_To_Crew_Expect_BelongToSomeCrewException() {
-        final String email = "Lambda";
-        Crew crew = textureFactory.makeCrew(false);
-        User user = textureFactory.makeUser(email,false);
-        CrewUser crewUser = textureFactory.makeCrewUser(crew, user,false);
+        final Long crewId = 1L;
+        final String email = "Lambda@Lambda.com";
 
-        //설정된 크루o ,Request 보낸적 없는 유저.
-        when(crewJoinRequestRepository.findCrewJoinRequestByCrewAndUser(any(Crew.class), any(User.class))).thenReturn(Optional.empty());
-        when(crewUserRepository.findByCrewAndUser(any(Crew.class), any(User.class))).thenReturn(Optional.of(crewUser));
+        Crew crew = textureFactory.makeCrew(false);
+        User user = textureFactory.makeUser(email, false);
+        CrewUser crewUser = textureFactory.makeCrewUser(crew, user, false);
+
+        crewJoinRequestRepositoryWillReturn(Optional.empty());
+        crewUserRepositoryWillReturn(Optional.of(crewUser));
+
+        crewRepositoryWillReturnCrew(crewId, crew);
+        userRepositoryWillReturnUser(email, user);
 
         assertThrows(BelongToSomeCrewException.class, () -> {
-            crewJoinRequestService.sendJoinRequest(crew, user);
+            crewJoinRequestService.sendJoinRequest(crewId, email);
         });
     }
 
     @Test
-    void When_CancelRequest_Expect_Call_delete(){
-        final String email = "Lambda";
+    void When_CancelRequest_Expect_Call_delete() {
+        final String email = "Lambda@Lambda.com";
         Crew crew = textureFactory.makeCrew(false);
-        User user = textureFactory.makeUser(email,false);
+        User user = textureFactory.makeUser(email, false);
 
-        CrewJoinRequest request= textureFactory.makeRequest(crew,user,false);
+        final Long requestId = 1L;
+        CrewJoinRequest request = textureFactory.makeRequest(crew, user, false);
 
-        when(crewJoinRequestRepository.findById(request.getId())).thenReturn(Optional.of(request));
+        when(crewJoinRequestRepository.findById(requestId)).thenReturn(Optional.of(request));
 
         //then
-        crewJoinRequestService.cancelJoinRequest(request);
-        verify(crewJoinRequestRepository,atLeastOnce()).delete(any(CrewJoinRequest.class));
+        crewJoinRequestService.cancelJoinRequest(requestId);
+        verify(crewJoinRequestRepository, atLeastOnce()).delete(any(CrewJoinRequest.class));
     }
 
     @Test
-    void When_acknowledgeRequest_Expect_Call_crewJoinRequest_Delete_crewUser_Save(){
-        final String email="Lambda";
-        Crew crew=textureFactory.makeCrew(false);
-        User user=textureFactory.makeUser(email,false);
+    void When_acknowledgeRequest_Expect_Call_crewJoinRequest_Delete_crewUser_Save() {
+        final String email = "Lambda@Lambda.com";
+        Crew crew = textureFactory.makeCrew(false);
+        User user = textureFactory.makeUser(email, false);
 
-        CrewJoinRequest request=textureFactory.makeRequest(crew,user,false);
+        final Long requestId = 1L;
+        CrewJoinRequest request = textureFactory.makeRequest(crew, user, false);
 
-        crewJoinRequestService.acknowledgeJoinRequest(request);
+        when(crewJoinRequestRepository.findById(requestId)).thenReturn(Optional.of(request));
 
-        verify(crewJoinRequestRepository,atLeastOnce()).delete(any(CrewJoinRequest.class));
-        verify(crewUserRepository,atLeastOnce()).save(any(CrewUser.class));
+        crewJoinRequestService.acknowledgeJoinRequest(requestId);
+
+        verify(crewJoinRequestRepository, atLeastOnce()).delete(any(CrewJoinRequest.class));
+        verify(crewUserRepository, atLeastOnce()).save(any(CrewUser.class));
     }
 
     Crew makeCrewWithGradeLimit(Grade grade) {
@@ -118,11 +142,26 @@ public class CrewJoinRequestServiceTest {
                 .build();
     }
 
-    User makeUserWithGrade(Grade grade) {
+    User makeUserWithGrade(String email, Grade grade) {
         User user = new User();
         user.setUsername("Lambda");
-        user.setEmail("Lambda@Lambda.com");
+        user.setEmail(email);
         user.setGrade(grade);
         return user;
+    }
+
+    void crewRepositoryWillReturnCrew(Long crewId, Crew crew) {
+        when(crewRepository.findById(crewId)).thenReturn(Optional.of(crew));
+    }
+
+    void userRepositoryWillReturnUser(String email, User user) {
+        when(userRepository.findByEmail(email)).thenReturn(user);
+    }
+
+    void crewJoinRequestRepositoryWillReturn(Optional<CrewJoinRequest>request){
+        when(crewJoinRequestRepository.findCrewJoinRequestByCrewAndUser(any(Crew.class), any(User.class))).thenReturn(request);
+    }
+    void crewUserRepositoryWillReturn(Optional<CrewUser>crewUser){
+        when(crewUserRepository.findByCrewAndUser(any(Crew.class), any(User.class))).thenReturn(crewUser);
     }
 }
