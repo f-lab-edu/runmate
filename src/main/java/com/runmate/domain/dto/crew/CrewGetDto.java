7ec4b70bc481db2d5fb4cd1e1ed673d67fdc5a86
bugdebugging <annyousung@gmail.com>
2021-05-20 16:45:00 +0900

    FIX:CrewQueryRepository.조회의 memberCount버그
    이미 Crew로 묶어버려서 CrewMemberCount는 제공할수가 없음.
    기능제공을 원할 경우 CrewMemberCount로 정렬한 조회 기능을 추가해야함.

--- src/main/java/com/runmate/domain/dto/crew/CrewGetDto.java
+++ src/main/java/com/runmate/domain/dto/crew/CrewGetDto.java
@@ -14,14 +14,12 @@ public class CrewGetDto {
     private Long id;
     private String name;
     private float totalDistance;
-    private Long memberCount;
     private LocalDateTime createdAt;
 
-    public CrewGetDto(Long id, String name, float totalDistance, Long memberCount, LocalDateTime createdAt) {
+    public CrewGetDto(Long id, String name, float totalDistance, LocalDateTime createdAt) {
         this.id = id;
         this.name = name;
         this.totalDistance = totalDistance;
-        this.memberCount = memberCount;
         this.createdAt = createdAt;
     }
 }
