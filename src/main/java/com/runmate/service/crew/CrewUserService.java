2c709e3d1ab3609005796ab9594d4d721d296fb5
bugdebugging <annyousung@gmail.com>
2021-05-25 16:23:14 +0900

    FIX:CrewUserService.탈퇴,제명 메소드 분리

--- src/main/java/com/runmate/service/crew/CrewUserService.java
+++ src/main/java/com/runmate/service/crew/CrewUserService.java
@@ -1,10 +1,14 @@
 package com.runmate.service.crew;
 
 import com.runmate.domain.crew.CrewUser;
+import com.runmate.domain.crew.Role;
 import com.runmate.domain.dto.crew.CrewUserGetDto;
+import com.runmate.domain.user.User;
 import com.runmate.repository.crew.CrewUserQueryRepository;
 import com.runmate.repository.crew.CrewUserRepository;
+import com.runmate.repository.user.UserRepository;
 import com.runmate.service.exception.NotFoundCrewUserException;
+import com.runmate.service.exception.UnAuthorizedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -17,14 +21,33 @@ import java.util.List;
 public class CrewUserService {
     private final CrewUserRepository crewUserRepository;
     private final CrewUserQueryRepository crewUserQueryRepository;
+    private final UserRepository userRepository;
 
     public List<CrewUserGetDto> searchCrewUser(Long crewId, int offset, int limit) {
         return crewUserQueryRepository.findCrewUserOrderByActivity(crewId, offset, limit);
     }
 
-    public void withDrawUser(Long crewUserId) {
-        CrewUser crewUser = crewUserRepository.findById(crewUserId)
-                .orElseThrow(NotFoundCrewUserException::new);
+    public void kickOutUser(Long adminCrewUserId, Long kickedCrewUserId) {
+        CrewUser adminCrewUser = getCrewUserInRepo(adminCrewUserId);
+        checkAdminUser(adminCrewUser);
+
+        CrewUser kickedCrewUser = getCrewUserInRepo(kickedCrewUserId);
+
+        crewUserRepository.delete(kickedCrewUser);
+    }
+
+    public void withDrawSelf(Long crewUserId) {
+        CrewUser crewUser = getCrewUserInRepo(crewUserId);
         crewUserRepository.delete(crewUser);
+    }
+
+    public CrewUser getCrewUserInRepo(Long crewUserId) {
+        return crewUserRepository.findById(crewUserId)
+                .orElseThrow(NotFoundCrewUserException::new);
+    }
+
+    void checkAdminUser(CrewUser crewUser) {
+        if (crewUser.getRole() != Role.ADMIN)
+            throw new UnAuthorizedException("you are not admin user");
     }
 }
