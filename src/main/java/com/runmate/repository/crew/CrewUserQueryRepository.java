a0b17c61fe06e57890434702678b3f16a75ce6d5
bugdebugging <annyousung@gmail.com>
2021-05-26 22:39:33 +0900

    FEAT:CrewUserQueryRepository 조회 조건 추가
    Distance의 합
    CreatedAt
    RunningTime의 합
    위의 세가지 결과를 기반으로
    오름차순,내림차순을 모두 구현
    
    Dto에서 전체 RunningTime의 합 제공하도록 추가

--- src/main/java/com/runmate/repository/crew/CrewUserQueryRepository.java
+++ src/main/java/com/runmate/repository/crew/CrewUserQueryRepository.java
@@ -1,48 +1,57 @@
 package com.runmate.repository.crew;
 
-import com.querydsl.core.Tuple;
 import com.querydsl.core.types.ConstructorExpression;
 import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
-import com.runmate.domain.activity.Activity;
-import com.runmate.domain.activity.QActivity;
-import com.runmate.domain.crew.Crew;
-import com.runmate.domain.crew.CrewUser;
-import com.runmate.domain.crew.QCrew;
-import com.runmate.domain.crew.QCrewUser;
 import com.runmate.domain.dto.crew.CrewUserGetDto;
-import com.runmate.domain.user.QUser;
-import com.runmate.domain.user.User;
+import com.runmate.repository.spec.CrewUserOrderSpec;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalTime;
 import java.util.List;
+
+import static com.runmate.domain.activity.QActivity.activity;
+import static com.runmate.domain.user.QUser.user;
+import static com.runmate.domain.crew.QCrew.crew;
+import static com.runmate.domain.crew.QCrewUser.crewUser;
 
 @Repository
 @RequiredArgsConstructor
 public class CrewUserQueryRepository {
     private final JPAQueryFactory queryFactory;
 
-    public List<CrewUserGetDto> findCrewUserOrderByActivity(Long crewId, int offset, int limit) {
-        QActivity activity = QActivity.activity;
-        QUser user = QUser.user;
-        QCrewUser crewUser = QCrewUser.crewUser;
-        QCrew crew = QCrew.crew;
-
-        ConstructorExpression<CrewUserGetDto> crewUserGetDtoConstructor = Projections.constructor(CrewUserGetDto.class,
-                crewUser.id, activity.distance.sum(), crewUser.role, crewUser.user.username, crewUser.createdAt);
-
-        return queryFactory.select(crewUserGetDtoConstructor)
+    public List<CrewUserGetDto> findCrewUserWithSorted(Long crewId, Pageable pageable, CrewUserOrderSpec orderSpec) {
+        return queryFactory.select(getCrewUserGetDtoConstructorExpression())
                 .from(activity)
                 .innerJoin(activity.user, user)
                 .innerJoin(user.crewUser, crewUser)
                 .innerJoin(crewUser.crew, crew)
                 .where(crew.id.eq(crewId))
                 .groupBy(crewUser)
-                .orderBy(activity.distance.sum().desc())
-                .offset(offset)
-                .limit(limit)
+                .orderBy(orderSpec.getSpecifier())
+                .offset(pageable.getOffset())
+                .limit(pageable.getPageSize())
                 .fetch();
+    }
+
+    private ConstructorExpression<CrewUserGetDto> getCrewUserGetDtoConstructorExpression() {
+        return Projections.constructor(CrewUserGetDto.class,
+                crewUser.id,
+                getSumDistance(),
+                crewUser.role,
+                crewUser.user.username,
+                crewUser.createdAt,
+                getSumSecondsOfRunningTime());
+    }
+
+    public static NumberExpression<Long> getSumSecondsOfRunningTime() {
+        return Expressions.numberTemplate(Long.class, "function('TIME_TO_SEC',{0})", activity.runningTime).sum();
+    }
+
+    public static NumberExpression<Float> getSumDistance() {
+        return activity.distance.sum();
     }
 }
