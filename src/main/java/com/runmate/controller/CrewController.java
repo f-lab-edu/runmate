86d4d1642057a385b6795356264a5e5df0c951c3
pkalsh <pkalsh345@gmail.com>
2021-05-29 17:35:31 +0900

    FEAT: Crew 생성 기능 API 구현
    - spring-hateoas 의존성 추가
    - request: POST "api/crews"
    - response: 201 CREATED
    - CrewService.createCrew 메서드가 DB에 저장한 엔터티를 반환하도록 수정

--- src/main/java/com/runmate/controller/CrewController.java
+++ src/main/java/com/runmate/controller/CrewController.java
@@ -1,4 +1,37 @@
 package com.runmate.controller;
 
+import com.runmate.domain.crew.Crew;
+import com.runmate.dto.crew.CrewPostDto;
+import com.runmate.service.crew.CrewService;
+import com.runmate.service.crew.CrewUserService;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/crews")
+@RestController
 public class CrewController {
+
+    private final CrewService crewService;
+    private final CrewUserService crewUserService;
+    private final ModelMapper modelMapper;
+
+    @PostMapping
+    public ResponseEntity<String> create(@RequestBody CrewPostDto crewPostDto) {
+        Crew crew = modelMapper.map(crewPostDto, Crew.class);
+        Crew savedCrew = crewService.createCrew(crew, crewPostDto.getEmail());
+        URI uri = WebMvcLinkBuilder.linkTo(CrewController.class).slash(savedCrew.getId()).toUri();
+        return ResponseEntity.created(uri).body("success");
+    }
+
+
+
 }
