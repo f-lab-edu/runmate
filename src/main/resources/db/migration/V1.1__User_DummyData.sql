a59d6862873fe6d3c12fef49592d87f19ab26abf
bugdebugging <annyousung@gmail.com>
2021-06-02 21:27:45 +0900

    FIX:Flyway Script split
    db 수정,추가 내용은 파일 추가하도록 하자

---
+++ src/main/resources/db/migration/V1.1__User_DummyData.sql
@@ -0,0 +1,21 @@
+#users
+insert into user(email, introduction, name, password, si, gu, grade)
+values ('you@you.com', '메일 뛰자!', 'you', '1234', 'seoul', 'nowon', 'BRONZE');
+
+insert into user(email, introduction, name, password, si, gu, grade)
+values ('ann@ann.com', '같이 뛰자!', 'ann', '1234', 'seoul', 'gangbuk', 'BRONZE');
+
+insert into user(email, introduction, name, password, si, gu, grade)
+values ('sung@sung.com', '오래 뛰자!', 'sung', '1234', 'seoul', 'gangnam', 'SILVER');
+
+insert into user(email, introduction, name, password, si, gu, grade)
+values ('min@gmail.com', '친구 구해요!', 'kim', '1234', 'seoul', 'nowon', 'BRONZE');
+
+insert into user(email, introduction, name, password, si, gu, grade)
+values ('jan@naver.com', '같이 뛰실분 쪽지 주세요', 'james', '1234', 'seoul', 'gangnam', 'BRONZE');
+
+insert into user(email, introduction, name, password, si, gu, grade)
+values ('one@gmail.com', '소개', 'one', '1234', 'seoul', 'mapo', 'SILVER');
+
+insert into user(email, introduction, name, password, si, gu, grade)
+values ('two@gmail.com', '소개', 'two', '1234', 'seoul', 'dongjak', 'UNRANKED');
