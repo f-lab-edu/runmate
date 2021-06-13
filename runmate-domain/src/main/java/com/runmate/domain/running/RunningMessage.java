3c0b63be8aada648190321cfa9105018958ef867
bugdebugging <annyousung@gmail.com>
2021-06-13 15:49:26 +0900

    FIX:RunningMessage memberId 필드 추가

--- runmate-domain/src/main/java/com/runmate/domain/running/RunningMessage.java
+++ runmate-domain/src/main/java/com/runmate/domain/running/RunningMessage.java
@@ -2,6 +2,7 @@ package com.runmate.domain.running;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -11,6 +12,7 @@ import java.time.LocalTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class RunningMessage {
     private Long teamId;
+    private Long memberId;
     private String username;
     private LocalTime averagePace;
     private LocalTime instantaneousPace;
@@ -18,8 +20,10 @@ public class RunningMessage {
     private Position position;
     private float distance;
 
-    public RunningMessage(Long teamId, String username, LocalTime averagePace, LocalTime instantaneousPace, Position position, float distance) {
+    @Builder
+    public RunningMessage(Long teamId, Long memberId, String username, LocalTime averagePace, LocalTime instantaneousPace, Position position, float distance) {
         this.teamId = teamId;
+        this.memberId = memberId;
         this.username = username;
         this.averagePace = averagePace;
         this.instantaneousPace = instantaneousPace;
