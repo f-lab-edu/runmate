6f80ed70e47e700890fda820db31db6cb9d893cf
bugdebugging <annyousung@gmail.com>
2021-06-15 15:09:09 +0900

    FEAT:목표 check 서비스

---
+++ runmate-websocket/src/main/java/com/runmate/service/JudgeRunningDataService.java
@@ -0,0 +1,34 @@
+package com.runmate.service;
+
+import com.runmate.domain.redis.TeamInfo;
+import com.runmate.exception.NotFoundTeamInfoException;
+import com.runmate.repository.redis.TeamInfoRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class JudgeRunningDataService {
+    private final TeamInfoRepository teamInfoRepository;
+
+    public boolean isTeamSuccessOnRunning(long teamId) {
+        TeamInfo teamInfo = findTeamInfo(teamId);
+        return teamInfo.isSuccessOnRunning();
+    }
+
+    public boolean isTeamTimeOver(long teamId) {
+        TeamInfo teamInfo = findTeamInfo(teamId);
+        return teamInfo.isTimeOver();
+    }
+
+    public boolean isTeamFailOnRunning(long teamId) {
+        TeamInfo teamInfo = findTeamInfo(teamId);
+        return teamInfo.isFailOnRunning();
+    }
+
+    private TeamInfo findTeamInfo(long teamId) {
+        return teamInfoRepository.findById(teamId).orElseThrow(NotFoundTeamInfoException::new);
+    }
+}
