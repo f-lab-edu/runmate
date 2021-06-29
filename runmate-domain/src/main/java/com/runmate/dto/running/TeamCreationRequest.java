c2fa6a6a586a2c48ab0843dfe2816d919f0d13eb
pkalsh <pkalsh345@gmail.com>
2021-06-29 21:51:40 +0900

    feat: DTO에 입력값 제약사항 설정

--- runmate-domain/src/main/java/com/runmate/dto/running/TeamCreationRequest.java
+++ runmate-domain/src/main/java/com/runmate/dto/running/TeamCreationRequest.java
@@ -1,17 +1,24 @@
 package com.runmate.dto.running;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @RequiredArgsConstructor
 public class TeamCreationRequest {
+    @NotNull
     private final long leaderId;
+    @NotBlank
     private final String title;
     private final List<String> emails;
+    @NotNull
     private final TeamGoalRequest goal;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     private final LocalDateTime startTime;
 }
--- runmate-domain/src/main/java/com/runmate/dto/running/TeamGoalRequest.java
+++ runmate-domain/src/main/java/com/runmate/dto/running/TeamGoalRequest.java
@@ -3,9 +3,13 @@ package com.runmate.dto.running;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
+import javax.validation.constraints.Positive;
+
 @Getter
 @RequiredArgsConstructor
 public class TeamGoalRequest {
+    @Positive
     private final float distance;
+    @Positive
     private final long runningTime;
 }
