7ebf9103fead7ccf45fc91c3080a230dbe4a7ecc
pkalsh <pkalsh345@gmail.com>
2021-06-29 09:40:13 +0900

    feat: 팀 생성 요청 api 구현
    기능: 팀 생성 후 초기 멤버들 추가
    응답 헤더: 201 Created
    redirected uri가 팀의 api를 가리킴
    응답 본문: 초대된 멤버들의 uri들을 담아서 전송

---
+++ runmate-api/src/main/java/com/runmate/controller/CrewRunningController.java
@@ -0,0 +1,49 @@
+package com.runmate.controller;
+
+import com.runmate.domain.running.Team;
+import com.runmate.domain.running.TeamMember;
+import com.runmate.dto.running.TeamCreationRequest;
+import com.runmate.service.crew.CrewRunningService;
+import com.runmate.utils.JsonWrapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
+@RequestMapping("/api/crew-running")
+@RestController
+public class CrewRunningController {
+
+    private final CrewRunningService crewRunningService;
+
+    @PostMapping
+    public ResponseEntity<JsonWrapper> createTeam(@RequestBody TeamCreationRequest request) {
+        Team team = crewRunningService.createTeam(request);
+        List<String> initialMemberEmails = request.getEmails();
+        List<URI> memberUris = initialMemberEmails.stream()
+                .map(email -> crewRunningService.addMember(team, email))
+                .map(teamMember -> buildTeamMemberUri(team, teamMember))
+                .collect(Collectors.toList());
+
+        JsonWrapper body = JsonWrapper.success(memberUris);
+        URI uri = WebMvcLinkBuilder.linkTo(CrewRunningController.class).slash("teams").slash(team.getId()).toUri();
+        return ResponseEntity.created(uri).body(body);
+    }
+
+    private URI buildTeamMemberUri(Team team, TeamMember teamMember) {
+        return WebMvcLinkBuilder.linkTo(CrewRunningController.class)
+                .slash("teams")
+                .slash(team.getId())
+                .slash("members")
+                .slash(teamMember.getId())
+                .toUri();
+    }
+}
